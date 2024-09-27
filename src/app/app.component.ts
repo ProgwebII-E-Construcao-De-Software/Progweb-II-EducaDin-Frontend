2a86323068dea893958b45b7fe97cfdf2901b81c
felipeKauaBarros <fkb475@gmail.com>
2024-09-27 08:25:33 -0300

    :ok_hand: atualizacao e mudança no home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,7 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {NavbarComponent} from "./components/navbar/navbar.component";
-import {FooterComponent} from "./components/footer/footer.component";
-import {HeaderComponent} from "./components/header/header.component";
-import {MatTableModule} from "@angular/material/table";
-import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
     selector: 'app-root',
-    standalone: true,
-    imports: [
-        RouterOutlet,
-        RouterLink,
-        NavbarComponent,
-        FooterComponent,
-        HeaderComponent,
-    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
+import {HomeComponent} from "./core/home/home.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
