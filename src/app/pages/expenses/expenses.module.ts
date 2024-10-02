d6d4544ee9d248aa128290a5e23728b2b084b64c
felipeKauaBarros <fkb475@gmail.com>
2024-10-02 05:29:44 -0300

    :boom: atualizacao no menu

--- src/app/pages/expenses/expenses.module.ts
+++ src/app/pages/expenses/expenses.module.ts
@@ -4,20 +4,27 @@ import {RouterModule} from "@angular/router";
 import {expensesRoutes} from "./expenses-routing.module";
 import {ExpensesHomeComponent} from "./expenses-home/expenses-home.component";
 import {ExpensesTableComponent} from "./expenses-table/expenses-table.component";
-import {MatButton} from "@angular/material/button";
-import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatIcon} from "@angular/material/icon";
+import {
+    MatCardActions,
+    MatCardContent,
+    MatCardHeader,
+    MatCardModule,
+    MatCardTitle
+} from "@angular/material/card";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {ExpensesDialogComponent} from "./expenses-dialog/expenses-dialog.component";
-import {MatCheckbox} from "@angular/material/checkbox";
-import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
-import {MatTable, MatTableModule} from "@angular/material/table";
+import {MatCheckbox, MatCheckboxModule} from "@angular/material/checkbox";
+import {MatMenu, MatMenuModule, MatMenuTrigger} from "@angular/material/menu";
+import {MatTableModule} from "@angular/material/table";
 import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
 import {MatDialogActions, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
 import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
+import {MatInput, MatInputModule} from "@angular/material/input";
 import {MatOption} from "@angular/material/autocomplete";
-import {MatSelect} from "@angular/material/select";
+import {MatSelect, MatSelectModule} from "@angular/material/select";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatOptionModule} from "@angular/material/core";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -29,15 +36,15 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     imports: [
         CommonModule,
         RouterModule.forChild(expensesRoutes),
-        MatButton,
-        MatCard,
+        MatCardModule,
         MatCardActions,
         MatCardContent,
         MatCardHeader,
         MatCardTitle,
-        MatIcon,
-        MatCheckbox,
-        MatMenu,
+        MatIconModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatMenuModule,
         MatMenuTrigger,
         MatTableModule,
         MatDatepicker,
@@ -47,10 +54,10 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
         MatDialogContent,
         MatDialogTitle,
         MatFormField,
-        MatInput,
+        MatInputModule,
         MatLabel,
-        MatOption,
-        MatSelect,
+        MatOptionModule,
+        MatSelectModule,
         MatSuffix,
         ReactiveFormsModule,
         FormsModule
