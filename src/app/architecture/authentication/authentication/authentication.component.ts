43e4d76de06df4dc894bbfb018f5c50e0fc98910
felipeKauaBarros <fkb475@gmail.com>
2024-11-19 12:12:30 -0300

    :construction: Login e Cadastro

--- src/app/architecture/authentication/authentication/authentication.component.ts
+++ src/app/architecture/authentication/authentication/authentication.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './authentication.component.scss'
 })
 export class AuthenticationComponent {
+  contentTransform: string = 'translateX(0)'; // Inicialmente mostra o painel de login
+  email: string = ''; // Armazena o email compartilhado entre os formulários
 
+  // Alterna para o painel de login
+  switchToLogin(): void {
+    this.contentTransform = 'translateX(0)';
+  }
+
+  // Alterna para o painel de cadastro
+  switchToRegister(): void {
+    this.contentTransform = 'translateX(-50%)';
+  }
+
+  // Simula o login
+  onLogin(): void {
+    console.log(`Usuário logado com o email: ${this.email}`);
+  }
+
+  // Simula o registro e preenche o email no login
+  onRegister(): void {
+    console.log(`Usuário cadastrado com o email: ${this.email}`);
+    this.switchToLogin(); // Após o cadastro, alterna para o painel de login
+  }
 }
