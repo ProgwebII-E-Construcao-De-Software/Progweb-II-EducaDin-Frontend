0c667444202328f24505aafc84bfc9f1daf50347
felipeKauaBarros <fkb475@gmail.com>
2024-11-21 11:59:24 -0300

    atualizacao

--- src/app/architecture/authentication/authentication/authentication.component.ts
+++ src/app/architecture/authentication/authentication/authentication.component.ts
@@ -1,32 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-authentication',
-  templateUrl: './authentication.component.html',
-  styleUrl: './authentication.component.scss'
+    selector: 'app-authentication',
+    templateUrl: './authentication.component.html',
+    styleUrl: './authentication.component.scss'
 })
 export class AuthenticationComponent {
-  contentTransform: string = 'translateX(0)'; // Inicialmente mostra o painel de login
-  email: string = ''; // Armazena o email compartilhado entre os formulários
+    ngOnInit() {
+        const container = document.getElementById('container');
+        const toggleForm = document.getElementById('toggleForm');
+        const cadastroForm = document.getElementById('cadastroForm');
+        const loginForm = document.getElementById('loginForm');
 
-  // Alterna para o painel de login
-  switchToLogin(): void {
-    this.contentTransform = 'translateX(0)';
-  }
-
-  // Alterna para o painel de cadastro
-  switchToRegister(): void {
-    this.contentTransform = 'translateX(-50%)';
-  }
-
-  // Simula o login
-  onLogin(): void {
-    console.log(`Usuário logado com o email: ${this.email}`);
-  }
-
-  // Simula o registro e preenche o email no login
-  onRegister(): void {
-    console.log(`Usuário cadastrado com o email: ${this.email}`);
-    this.switchToLogin(); // Após o cadastro, alterna para o painel de login
-  }
+        toggleForm?.addEventListener('click', () => {
+            container?.classList.toggle('show-login');
+            // Alterna entre mostrar Cadastro e Login
+            if (container?.classList.contains('show-login')) {
+                cadastroForm?.setAttribute('style', 'display: none;');
+                loginForm?.setAttribute('style', 'display: block;');
+                toggleForm!.textContent = 'Cadastrar';
+            } else {
+                loginForm?.setAttribute('style', 'display: none;');
+                cadastroForm?.setAttribute('style', 'display: block;');
+                toggleForm!.textContent = 'Entrar';
+            }
+        });
+    }
 }
